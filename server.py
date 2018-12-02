01197ef4ad0f75093b5d9009f805769568dbb140
Aksana <kuzmitskaya.aksana@bcgplatinion.com>
2018-12-02 10:37:58 -0800

    model tested

--- server.py
+++ server.py
@@ -1,22 +1,53 @@
 from flask import Flask, jsonify, request
+from sklearn.feature_extraction.text import CountVectorizer
+import pickle
 
 app = Flask(__name__)
 
+sugar_model = None
+carbs_model = None
+tfidf_vect = None
+
+def init():
+    global inputs_dc, prediction_dc
+    from sklearn.externals import joblib
+ 
+    features = ['_269 Sugars, total (g)', '_205 Carbohydrate (g)']
+
+    # Load models
+    global sugar_model
+    global carbs_model
+    global tfidf_vect
+    sugar_model = joblib.load('./nutritionSearch/ipython/output/{0}_model.pkl'.format(features[0]))
+    carbs_model = joblib.load('./nutritionSearch/ipython/output/{0}_model.pkl'.format(features[1]))
+    tfidf_vect = CountVectorizer(decode_error="replace",vocabulary=pickle.load(
+                                open("./nutritionSearch/ipython/output/feature.pkl", "rb")))
+
+    
+def run(input_text):    
+    x_tfidf = tfidf_vect.transform(input_text)
+
+    sugar_pred = sugar_model.predict(x_tfidf)
+    carbs_pred = carbs_model.predict(x_tfidf)
+
+    return [sugar_pred, carbs_pred]
 
 @app.route('/')
 def hello_world():
     return 'Welcome to EatGeek!'
 
-
 @app.route('/nutrition')
 def nutrition():
+    init()
+
     description = request.args.get('description') or request.args.get('desciption')
 
-    return {
-        "Loaded Fries queso monterey jack cheddar bacon green onions bbq ranch"jsonify({"sugar": "6 g", "carbohydrates": "95 g"}),
-        "Spinach Florentine Flatbread spinach artichoke hearts monterey jack parmesan romano tomatoes garlic fresh basil crushed red pepper oregano": jsonify({"sugar": "4 g", "carbohydrates": "51 g"}),
-        "BBQ Chicken Flatbread chipotle chicken bbq sauce monterey jack cheddar cilantro red peppers red onions": jsonify({"sugar": "18 g", "carbohydrates": "66 g"}),
-        "Crispy Brussels Sprouts lemon soy vinaigrette roasted onions croutons cotija cheese": jsonify({"sugar": "8 g", "carbohydrates": "38 g"}),
-        "Giant Onion Rings bbq ranch": jsonify({"sugar": "33 g", "carbohydrates": "155 g"}),
-        "none": jsonify({"sugar": "0-7 g", "carbohydrates": "0-10 g"})
-    }[description or "none"]
+    sugar_pred, carbs_pred = run([description])
+    
+    sugar_ranges = {1: '0-7 g', 2: '8-19 g', 3: '20-35 g', 4: '36-58 g', 5: '59-100 g'}
+    carbs_ranges = {1: '0-10 g', 2: '11-24 g', 3: '25-43 g', 4: '44-66 g', 5: '67-100 g'}
+
+    sugar_pred = sugar_ranges[sugar_pred[0]]
+    carbs_pred = carbs_ranges[carbs_pred[0]]
+
+    return jsonify({"sugar": sugar_pred, "carbohydrates": carbs_pred})
